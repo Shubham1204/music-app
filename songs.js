5af4a0332b3bfc244172635296a37cee4061e32a
Shubham Gupta <32667635+Shubham1204@users.noreply.github.com>
2019-09-29 23:29:13 +0530

    code

---
+++ songs.js
@@ -0,0 +1,92 @@
+var songsArray = [{
+    "id": "101",
+    "name": "Bang Bang",
+    "imageUrl": "https://m.media-amazon.com/images/M/MV5BZjJjN2IyMGYtNzYwNS00YzFhLTg2N2MtMmZhNjFkNmUyYmI0XkEyXkFqcGdeQXVyNDkxMzY0Mjk@._V1_.jpg",
+    "songUrl": "songs/bang.mp3"
+},
+{
+    "id": "102",
+    "name": "Faded",
+    "imageUrl": "https://m.media-amazon.com/images/M/MV5BZTUyYWI4Y2EtZGQ2MC00NDNiLTg3N2UtMWMwY2I1ZTViYzA4XkEyXkFqcGdeQXVyMTY5MDE5NA@@._V1_UY268_CR43,0,182,268_AL_.jpg",
+    "songUrl": "songs/faded.mp3"
+},
+{
+    "id": "103",
+    "name": "Galti.mp3",
+    "imageUrl": "https://www.pagalworld.io/GpE34Kg9Gq/8235/thumb-new-collection-3-300.jpg",
+    "songUrl": "songs/galti.mp3",
+},
+{
+    "id": "104",
+    "name": "Na Na",
+    "imageUrl": "https://www.androidmobilezone.com/protected/uploads/media/cover/Na-Na-Na-song-poster-Karan-Aujla.jpg",
+    "songUrl": "songs/nana.mp3",
+},
+{
+    "id": "105",
+    "name": "Vedalam Theme",
+    "imageUrl": "https://www.kollywoodzone.com/data/media/8392/thala_ajith_vedalam_posters_01.jpg",
+    "songUrl": "songs/vedalam.mp3",
+},
+{
+    "id": "106",
+    "name": "Bang Bang",
+    "imageUrl": "https://m.media-amazon.com/images/M/MV5BZjJjN2IyMGYtNzYwNS00YzFhLTg2N2MtMmZhNjFkNmUyYmI0XkEyXkFqcGdeQXVyNDkxMzY0Mjk@._V1_.jpg",
+    "songUrl": "songs/bang.mp3"
+},
+{
+    "id": "107",
+    "name": "Faded",
+    "imageUrl": "https://m.media-amazon.com/images/M/MV5BZTUyYWI4Y2EtZGQ2MC00NDNiLTg3N2UtMWMwY2I1ZTViYzA4XkEyXkFqcGdeQXVyMTY5MDE5NA@@._V1_UY268_CR43,0,182,268_AL_.jpg",
+    "songUrl": "songs/faded.mp3"
+},
+{
+    "id": "108",
+    "name": "Jagga Jasoos Galti",
+    "imageUrl": "http://a10.gaanacdn.com/images/song/82/21824382/crop_480x480_1516001759.jpg",
+    "songUrl": "songs/jaga.mp3",
+},
+{
+    "id": "109",
+    "name": "Na Na",
+    "imageUrl": "https://www.androidmobilezone.com/protected/uploads/media/cover/Na-Na-Na-song-poster-Karan-Aujla.jpg",
+    "songUrl": "songs/nana.mp3",
+},
+{
+    "id": "110",
+    "name": "Vedalam Theme",
+    "imageUrl": "https://www.kollywoodzone.com/data/media/8392/thala_ajith_vedalam_posters_01.jpg",
+    "songUrl": "songs/vedalam.mp3",
+},
+{
+    "id": "111",
+    "name": "Bang Bang",
+    "imageUrl": "https://m.media-amazon.com/images/M/MV5BZjJjN2IyMGYtNzYwNS00YzFhLTg2N2MtMmZhNjFkNmUyYmI0XkEyXkFqcGdeQXVyNDkxMzY0Mjk@._V1_.jpg",
+    "songUrl": "songs/bang.mp3"
+},
+{
+    "id": "112",
+    "name": "Faded",
+    "imageUrl": "https://m.media-amazon.com/images/M/MV5BZTUyYWI4Y2EtZGQ2MC00NDNiLTg3N2UtMWMwY2I1ZTViYzA4XkEyXkFqcGdeQXVyMTY5MDE5NA@@._V1_UY268_CR43,0,182,268_AL_.jpg",
+    "songUrl": "songs/faded.mp3"
+},
+{
+    "id": "113",
+    "name": "Galti.mp3",
+    "imageUrl": "https://www.pagalworld.io/GpE34Kg9Gq/8235/thumb-new-collection-3-300.jpg",
+    "songUrl": "songs/galti.mp3",
+},
+{
+    "id": "114",
+    "name": "Na Na",
+    "imageUrl": "https://www.androidmobilezone.com/protected/uploads/media/cover/Na-Na-Na-song-poster-Karan-Aujla.jpg",
+    "songUrl": "songs/nana.mp3",
+},
+{
+    "id": "115",
+    "name": "Vedalam Theme",
+    "imageUrl": "https://www.kollywoodzone.com/data/media/8392/thala_ajith_vedalam_posters_01.jpg",
+    "songUrl": "songs/vedalam.mp3",
+},
+
+]
